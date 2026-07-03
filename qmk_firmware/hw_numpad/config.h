a8ab20b08578f7a5084025ac94674dfa2badf12e
Cameron Hope <62604718+CameronHope1@users.noreply.github.com>
2026-07-03 14:20:47 +1000

    Add project files to repo

---
+++ qmk_firmware/hw_numpad/config.h
@@ -0,0 +1,6 @@
+#pragma once
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 4
+
+#define DIODE_DIRECTION COL2ROW
+#define DEBOUNCE 5
---
+++ qmk_firmware/hw_numpad/keymaps/via/keymap.c
@@ -0,0 +1,99 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │TG1│ F1│ F2│ F3│ 
+     * ┌───┬───┬───┬───┐
+     * │NUM│ / │ * │ - │
+     * ├───┼───┼───┼───┤
+     * │ 7 │ 8 │ 9 │   │
+     * ├───┼───┼───┤ + │
+     * │ 4 │ 5 │ 6 │   │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │   │
+     * ├───┴───┼───┤Ent│
+     * │   0   │ . │   │
+     * └───────┴───┴───┘
+     */
+    [0] = LAYOUT_numpad_6x4(
+        TG(1),   KC_F1, KC_F2, KC_F3,
+        KC_NUM_LOCK,   KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9, KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,   
+        KC_P1,   KC_P2,   KC_P3, KC_PENT,
+        KC_P0,      KC_PDOT          
+    ),
+
+    /* ┌───┬───┬───┬───┐
+     * │TG1│F1 │ F2│ F3│
+     * ┌───┬───┬───┬───┐
+     * │NUM│ / │ * │ - │
+     * ┌───┬───┬───┐───┤
+     * │Hom│ ↑ │PgU│   │
+     * ├───┼───┼───┤ + │
+     * │ ← │   │ → │   │
+     * ├───┼───┼───┤───┤
+     * │End│ ↓ │PgD│   │
+     * ├───┴───┼───┤Ent│
+     * │Insert │Del│   │
+     * └───────┴───┘───┘
+     */
+    [1] = LAYOUT_numpad_6x4(
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        KC_HOME, KC_UP,   KC_PGUP, _______,
+        KC_LEFT, XXXXXXX, KC_RGHT, 
+        KC_END,  KC_DOWN, KC_PGDN, _______,
+        KC_INS,     KC_DEL
+    ),
+
+    /* ┌───┬───┬───┬───┐
+     * │TG1│F1 │ F2│ F3│
+     * ┌───┬───┬───┬───┐
+     * │NUM│ / │ * │ - │
+     * ┌───┬───┬───┐───┤
+     * │Hom│ ↑ │PgU│   │
+     * ├───┼───┼───┤ + │
+     * │ ← │   │ → │   │
+     * ├───┼───┼───┤───┤
+     * │End│ ↓ │PgD│   │
+     * ├───┴───┼───┤Ent│
+     * │Insert │Del│   │
+     * └───────┴───┘───┘
+     */
+    [2] = LAYOUT_numpad_6x4(
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        KC_HOME, KC_UP,   KC_PGUP, _______,
+        KC_LEFT, XXXXXXX, KC_RGHT, 
+        KC_END,  KC_DOWN, KC_PGDN, _______,
+        KC_INS,     KC_DEL
+    ),
+
+    /* ┌───┬───┬───┬───┐
+     * │TG1│F1 │ F2│ F3│
+     * ┌───┬───┬───┬───┐
+     * │NUM│ / │ * │ - │
+     * ┌───┬───┬───┐───┤
+     * │Hom│ ↑ │PgU│   │
+     * ├───┼───┼───┤ + │
+     * │ ← │   │ → │   │
+     * ├───┼───┼───┤───┤
+     * │End│ ↓ │PgD│   │
+     * ├───┴───┼───┤Ent│
+     * │Insert │Del│   │
+     * └───────┴───┘───┘
+     */
+    [3] = LAYOUT_numpad_6x4(
+        _______, _______, _______, _______,
+        _______, _______, _______, _______,
+        KC_HOME, KC_UP,   KC_PGUP, _______,
+        KC_LEFT, XXXXXXX, KC_RGHT, 
+        KC_END,  KC_DOWN, KC_PGDN, _______,
+        KC_INS,     KC_DEL
+    )
+};
